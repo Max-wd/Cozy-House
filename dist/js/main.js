88b45ccc97e795d0f469747aeb8a76435679b4e3
Maksim Stupitskiy <stupitskiy.maksim@gmail.com>
2020-05-12 20:18:43 +0300

    Added Cozy House

---
+++ dist/js/main.js
@@ -0,0 +1,6 @@
+const element = document.getElementsByClassName("navbar-toggle");
+element[0].addEventListener("click", function(event) {
+  event.preventDefault();
+  element[0].classList.toggle("is-active");
+});
+//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiIsImZpbGUiOiJtYWluLmpzIiwic291cmNlc0NvbnRlbnQiOltdfQ==
